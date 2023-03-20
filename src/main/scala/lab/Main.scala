45bcbac17ba86c5cbd3c5d2861976dd7a552f76a
Mattia Matteini <matteinimattia@gmail.com>
2023-03-20 17:48:59 +0100

    feat: added append method

--- src/main/scala/lab/Main.scala
+++ src/main/scala/lab/Main.scala
@@ -1,5 +1,7 @@
 package lab
 
+import scala.annotation.tailrec
+
 object Main extends App:
 
   import u03.Lists.List
@@ -9,7 +11,18 @@ object Main extends App:
   import Option.*
 
   // Task 1
+  @tailrec
   def drop[A](l: List[A], n: Int): List[A] = (l, n) match
     case (Nil(), _) => Nil()
     case (Cons(h, t), 0) => Cons(h, t)
     case (Cons(_, t), i) => drop(t, i - 1)
+
+  //  private def get[A](l: List[A], i: Int): Option[A] = (l, i) match
+  //    case (Nil(), _) => None()
+  //    case (Cons(h, _), 0) => Some(h)
+  //    case (Cons(h, t), i) => get(Cons(h, t), i - 1)
+
+  def append[A](left: List[A], right: List[A]): List[A] = (left, right) match
+    case (Nil(), r) => r
+    case (l, Nil()) => l
+    case (Cons(h, t), r) => Cons(h, append(t, r))
--- src/test/scala/lab/MainTest.scala
+++ src/test/scala/lab/MainTest.scala
@@ -10,9 +10,16 @@ import List.*
 class MainTest:
 
   val lst = Cons(10, Cons(20, Cons(30, Nil())))
+  val tail = Cons(40, Nil())
 
   @Test def testDrop() =
     assertEquals(Cons(20, Cons(30, Nil())), drop(lst, 1))
     assertEquals(Cons(30, Nil()), drop(lst, 2))
     assertEquals(Nil(), drop(lst, 5))
+
+  @Test def testAppend() =
+    assertEquals(Cons(10, Cons(20, Cons(30, Cons(40, Nil())))), append(lst, tail))
+    assertEquals(Cons(40, Nil()), append(Nil(), tail))
+
+
 
