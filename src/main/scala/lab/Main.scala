c558517b30d6c4c8be15cd8234e9267ca6ce4158
Mattia Matteini <matteinimattia@gmail.com>
2023-03-24 18:29:15 +0100

    feat: finished tasks

--- src/main/scala/lab/Main.scala
+++ src/main/scala/lab/Main.scala
@@ -49,5 +49,54 @@ object Main extends App:
     case Nil() => acc
     case Cons(head, tail) => f(head, foldRight(tail)(acc)(f))
 
-// attempt gone wrong D:
-//  def foldRight(list: List[Int])(acc: Int)(f: (Int, Int) => Int): Int =  foldLeft(list)(acc)((a, b) => f(b, a))
+  // attempt gone wrong D:
+  // need reverse function
+  //  def foldRight(list: List[Int])(acc: Int)(f: (Int, Int) => Int): Int =  foldLeft(list)(acc)((a, b) => f(b, a))
+
+
+  // Task 3
+  enum Stream[A]:
+    private case Empty()
+    private case Cons(head: () => A, tail: () => Stream[A])
+
+  object Stream:
+
+    def empty[A](): Stream[A] = Empty()
+
+    def cons[A](hd: => A, tl: => Stream[A]): Stream[A] =
+      lazy val head = hd
+      lazy val tail = tl
+      Cons(() => head, () => tail)
+
+    def toList[A](stream: Stream[A]): List[A] = stream match
+      case Cons(h, t) => List.Cons(h(), toList(t()))
+      case _ => List.Nil()
+
+    def map[A, B](stream: Stream[A])(f: A => B): Stream[B] = stream match
+      case Cons(head, tail) => cons(f(head()), map(tail())(f))
+      case _ => Empty()
+
+    def filter[A](stream: Stream[A])(pred: A => Boolean): Stream[A] = stream match
+      case Cons(head, tail) if pred(head()) => cons(head(), filter(tail())(pred))
+      case Cons(_, tail) => filter(tail())(pred)
+      case _ => Empty()
+
+    def take[A](stream: Stream[A])(n: Int): Stream[A] = (stream, n) match
+      case (Cons(head, tail), n) if n > 0 => cons(head(), take(tail())(n - 1))
+      case _ => Empty()
+
+    def iterate[A](init: => A)(next: A => A): Stream[A] = cons(init, iterate(next(init))(next))
+
+    @tailrec
+    def drop[A](stream: Stream[A])(n: Int): Stream[A] = (stream, n) match
+      case (Empty(), _) => Empty()
+      case (Cons(h, t), 0) => Cons(h, t)
+      case (Cons(_, t), i) => drop(t())(i - 1)
+
+    def constant[A](x: A): Stream[A] = cons(x, constant(x))
+
+
+    // it is not all my own work...
+    def fibs(): Stream[Int] =
+      def fibonacci(a: Int, b: Int): Stream[Int] = cons(a, fibonacci(b, a + b))
+      fibonacci(0, 1)
--- src/test/scala/lab/MainTest.scala
+++ src/test/scala/lab/MainTest.scala
@@ -9,23 +9,23 @@ import List.*
 
 class MainTest:
 
-  val lst = Cons(10, Cons(20, Cons(30, Nil())))
-  val tail = Cons(40, Nil())
+  val lst: List[Int] = Cons(10, Cons(20, Cons(30, Nil())))
+  val tail: List[Int] = Cons(40, Nil())
 
-  @Test def testDrop() =
+  @Test def testDrop(): Unit =
     assertEquals(Cons(20, Cons(30, Nil())), drop(lst, 1))
     assertEquals(Cons(30, Nil()), drop(lst, 2))
     assertEquals(Nil(), drop(lst, 5))
 
-  @Test def testAppend() =
+  @Test def testAppend(): Unit =
     assertEquals(Cons(10, Cons(20, Cons(30, Cons(40, Nil())))), append(lst, tail))
     assertEquals(Cons(40, Nil()), append(Nil(), tail))
 
-  @Test def testFlatMap() =
+  @Test def testFlatMap(): Unit =
     assertEquals(Cons(11, Cons(21, Cons(31, Nil()))), flatMap(lst)(v => Cons(v + 1, Nil())))
     assertEquals(Cons(11, Cons(12, Cons(21, Cons(22, Cons(31, Cons(32, Nil())))))), flatMap(lst)(v => Cons(v + 1, Cons(v + 2, Nil()))))
 
-  @Test def testMax() =
+  @Test def testMax(): Unit =
     import u02.Optionals.Option
     import Option.*
     assertEquals(Some(25), max(Cons(10, Cons(25, Cons(20, Nil())))))
@@ -33,17 +33,28 @@ class MainTest:
     assertEquals(Some(1), max(Cons(1, Nil())))
     assertEquals(None(), max(Nil()))
 
-  @Test def testGetCourses() =
+  @Test def testGetCourses(): Unit =
     import u02.Modules.Person
     import Person.*
     val persons = Cons(Student("Matteini", 22), Cons(Teacher("Viroli", "pps"), Cons(Student("Matteini", 22), Nil())))
     assertEquals(Cons("pps", Nil()), getCourses(persons))
     assertEquals(Nil(), getCourses(Cons(Student("Matteini", 22), Nil())))
 
-  @Test def testFolds() =
+  @Test def testFolds(): Unit =
     val lst = Cons(3, Cons(7, Cons(1, Cons(5, Nil()))))
     assertEquals(0, foldLeft(Nil())(0)(_ - _))
     assertEquals(-16, foldLeft(lst)(0)(_ - _))
     assertEquals(-8, foldRight(lst)(0)(_ - _))
     assertEquals(16, foldRight(lst)(0)(_ + _))
 
+  @Test def testTake(): Unit =
+    val s = Stream.take(Stream.iterate(0)(_ + 1))(10)
+    assertEquals(Cons(6, Cons(7, Cons(8, Cons(9, Nil())))), Stream.toList(Stream.drop(s)(6)))
+
+  @Test def testConstant(): Unit =
+    assertEquals(Cons("x", Cons("x", Cons("x", Cons("x", Cons("x", Nil()))))), Stream.toList(Stream.take(Stream.constant("x"))(5)))
+    assertEquals(Cons(1, Cons(1, Cons(1, Cons(1, Cons(1, Nil()))))), Stream.toList(Stream.take(Stream.constant(1))(5)))
+
+  @Test def testFibs(): Unit =
+    assertEquals(Cons(0, Cons(1, Cons(1, Cons(2, Cons(3, Cons(5, Cons(8, Cons(13, Nil())))))))),
+      Stream.toList(Stream.take(Stream.fibs())(8)))
